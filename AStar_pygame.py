4432ecc90df63c0dfdc54c3e15464bffc4f9334a
Nanook <56505924+EntenDNA@users.noreply.github.com>
2020-01-25 17:03:35 +0100

    Improved Code

--- AStar_pygame.py
+++ AStar_pygame.py
@@ -29,7 +29,6 @@ def get_neighbours(cnode):
 
     for tup_mask in mask:
 
-        # tupel
         addx, addy = tup_mask
         addedx, addedy = addx + cnode.x, addy + cnode.y
 
@@ -58,6 +57,67 @@ def retrace(startn, endn):
     path.reverse()
     return path
 
+def Astar(gröse):
+        while True:
+            clock.tick(30)
+            try:
+                current = opened[0]
+            except:
+                print("No way found")
+                found = False
+                break
+
+            for i in range(len(opened)):
+
+                if (opened[i].f < current.f) or (opened[i].f == current.f and opened[i].h < current.h):
+                    current = opened[i]
+
+            opened.remove(current)
+            closed.append(current)
+
+            if (current.y, current.x) not in [(start.y, start.x), (end.y, end.x)]:
+                pygame.draw.rect(screen, brown, (current.x*gröse+(current.x*1)+1, current.y*gröse+(current.y*1)+1, gröse, gröse))
+                pygame.display.update()
+
+            if (current.x, current.y) == (end.x, end.y):
+                end.parent = current
+                found = True
+                retrace(start, end)
+                for node in path[:-2]:
+                    y, x = node.y, node.x
+                    pygame.draw.rect(screen, blue, (x*gröse+(x*1)+1, y*gröse+(y*1)+1, gröse, gröse))
+                    pygame.display.update()
+                    pygame.time.wait(30)
+
+                break
+
+            closed_cords = set()
+
+            for closd in closed:
+                closed_cords.add((closd.y, closd.x))
+
+            for neighbour in get_neighbours(current):
+                if (neighbour.y, neighbour.x) in closed_cords:
+                    continue
+
+                newMovementCost = current.g + distance(current, neighbour)
+                if newMovementCost < neighbour.g or neighbour not in opened:
+                    neighbour.g = newMovementCost
+                    neighbour.h = distance(neighbour, end)
+                    neighbour.f = neighbour.g + neighbour.h
+                    neighbour.parent = current
+
+                    opened_cords = set()
+
+                    for opend in opened:
+                        opened_cords.add((opend.y, opend.x))
+
+                    if (neighbour.y, neighbour.x) not in opened_cords and field[neighbour.y][neighbour.x] != "X":
+                        opened.append(neighbour)
+                        if (neighbour.y, neighbour.x) != (end.y, end.x):
+                            pygame.draw.rect(screen, white2, (neighbour.x*gröse+(neighbour.x*1)+1, neighbour.y*gröse+(neighbour.y*1)+1, gröse, gröse))
+                            pygame.display.update()
+
 start = Node(1,1)
 end = Node(93,79)
 
@@ -69,11 +129,11 @@ y = 1
 LEFT = 1
 RIGHT = 3
 
-weis = (255,255,255)
-schwarz = (0,0,0)
-grün = (50,205,50)
-rot = (255,0,0)
-blau = (0,0,255)
+white = (255,255,255)
+black = (0,0,0)
+green = (50,205,50)
+red = (255,0,0)
+blue = (0,0,255)
 white2 = (255,235,205)
 brown = (139,69,19)
 
@@ -86,6 +146,7 @@ closed = []
 width = fgröse * gröse + (fgröse - 1) * 1
 height = fgröse * gröse + (fgröse - 1) * 1
 screen = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
 
 opened.append(start)
 
@@ -101,90 +162,25 @@ while True:
                 mx = int((mx-(int(mx/gröse)*1))/gröse)
                 my = int((my-(int(my/gröse)*1))/gröse)
 
-                if field[my][mx] == "O" and pygame.Surface.get_at(screen, pos) != schwarz:
-                    pygame.draw.rect(screen, schwarz, (mx*gröse+(mx*1)+1, my*gröse+(my*1)+1, gröse, gröse))
+                if field[my][mx] == "O" and pygame.Surface.get_at(screen, pos) != black:
+                    pygame.draw.rect(screen, black, (mx*gröse+(mx*1)+1, my*gröse+(my*1)+1, gröse, gröse))
                     field[my][mx] = "X"
 
-                """elif field[my][mx] == "X":
-                    field[my][mx] = "O"
-                    pygame.draw.rect(screen, weis, (mx*gröse+(mx*1)+1, my*gröse+(my*1)+1, gröse, gröse))"""
             except:
                 pass
 
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == RIGHT:
-            while True:
-                pygame.time.wait(1)
-
-                global current
-                try:
-                    current = opened[0]
-                except:
-                    print("No way found")
-                    found = False
-                    break
-
-                for i in range(len(opened)):
-
-                    if (opened[i].f < current.f) or (opened[i].f == current.f and opened[i].h < current.h):
-                        current = opened[i]
-
-                opened.remove(current)
-                closed.append(current)
-
-                if (current.y, current.x) not in [(start.y, start.x), (end.y, end.x)]:
-                    pygame.draw.rect(screen, brown, (current.x*gröse+(current.x*1)+1, current.y*gröse+(current.y*1)+1, gröse, gröse))
-                    pygame.display.update()
-                    pygame.time.wait(10)
-
-                if (current.x, current.y) == (end.x, end.y):
-                    end.parent = current
-                    found = True
-                    retrace(start, end)
-                    for node in path[:-2]:
-                        y, x = node.y, node.x
-                        pygame.draw.rect(screen, blau, (x*gröse+(x*1)+1, y*gröse+(y*1)+1, gröse, gröse))
-                        pygame.display.update()
-                        pygame.time.wait(30)
-
-                    break
-
-                closed_cords = set()
-
-                for closd in closed:
-                    closed_cords.add((closd.y, closd.x))
-
-                for neighbour in get_neighbours(current):
-                    if (neighbour.y, neighbour.x) in closed_cords:
-                        continue
-
-                    newMovementCost = current.g + distance(current, neighbour)
-                    if newMovementCost < neighbour.g or neighbour not in opened:
-                        neighbour.g = newMovementCost
-                        neighbour.h = distance(neighbour, end)
-                        neighbour.f = neighbour.g + neighbour.h
-                        neighbour.parent = current
-
-                        opened_cords = set()
-
-                        for opend in opened:
-                            opened_cords.add((opend.y, opend.x))
-
-                        if (neighbour.y, neighbour.x) not in opened_cords and field[neighbour.y][neighbour.x] != "X":
-                            opened.append(neighbour)
-                            if (neighbour.y, neighbour.x) != (end.y, end.x):
-                                pygame.draw.rect(screen, white2, (neighbour.x*gröse+(neighbour.x*1)+1, neighbour.y*gröse+(neighbour.y*1)+1, gröse, gröse))
-                                pygame.display.update()
-                                pygame.time.wait(10)
+            Astar(gröse)
 
     while z != fgröse - 1 and s != fgröse - 1:
         for z in range(len(field)):
             for s in range(len(field[z])):
                 if field[z][s] == "S":
-                    pygame.draw.rect(screen, grün, (x, y, gröse, gröse))
+                    pygame.draw.rect(screen, green, (x, y, gröse, gröse))
                 elif field[z][s] == "E":
-                    pygame.draw.rect(screen, rot, (x, y, gröse, gröse))
+                    pygame.draw.rect(screen, red, (x, y, gröse, gröse))
                 else:
-                    pygame.draw.rect(screen, weis, (x, y, gröse, gröse))
+                    pygame.draw.rect(screen, white, (x, y, gröse, gröse))
 
                 y = y+gröse+1 if s == fgröse-1 else y
                 x = x+gröse+1 if s != fgröse-1 else 1
